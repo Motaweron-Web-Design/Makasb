e83d7f62162e7f8fc770acec2f1b2d2a4caff0ba
Ahmed Eltatawe <eltatawea@gmail.com>
2022-06-22 09:44:17 +0200

    LAST ADD 22 6

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,73 @@
 $(document).ready(function () {
-    $(".navTopLink li a").click(function () {
-        $(this).addClass("active").parent().siblings().find("a").removeClass("active")
-        $("body , html").animate({
-            scrollTop: $("#" + $(this).data("scroll")).offset().top -200
-         } , 100)
-    })
-    $(".linkSocial ul li").on("click" , function () {
-        $(this).addClass("active").siblings().removeClass("active")
-        $("#" + $(this).data("show")).fadeIn(500).siblings().hide()
-    })
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 250) {
-          $(".fa-angle-up").show()
-        } else {
-          $(".fa-angle-up").fadeOut()
-        }
-      })
-      $(".fa-angle-up").click(function () {
-        scroll({
-          top: 0,
-          left: 0,
-          behavior: "smooth",
-        });
-      })
-      $("body").css("paddingTop" , $("header .nav2").innerHeight())
-      $(window).scroll(function () {
-        if ($(window).scrollTop() >= 66) {
-            $("header .nav2").addClass("active")
-            $(".LogoNav2").addClass("active")
-        } else {
-            $("header .nav2").removeClass("active")
-            $(".LogoNav2").removeClass("active")
-        }
-      })
-      $(window).on("load" , function () {
-        $("#email").focus()
-      })
+  $(".navTopLink li a:not(:last)").click(function () {
+    $(this)
+      .addClass("active")
+      .parent()
+      .siblings()
+      .find("a")
+      .removeClass("active");
+    $("body , html").animate(
+      {
+        scrollTop: $("#" + $(this).data("scroll")).offset().top - 200,
+      },
+      100
+    );
+  });
+  $(".linkSocial .mainButton2").on("click", function (e) {
+    e.preventDefault();
+    $(this).addClass("active").siblings().removeClass("active");
+    $("#" + $(this).data("show"))
+      .fadeIn(500)
+      .siblings()
+      .hide();
+    console.log($("#" + $(this).data("show")));
+  });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 250) {
+      $(".fa-angle-up").show();
+    } else {
+      $(".fa-angle-up").fadeOut();
+    }
+  });
+  $(".fa-angle-up").click(function () {
+    scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
 
-})
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 66) {
+      $("body").css("paddingTop", $("header .nav2").innerHeight());
+      $("header .nav2").addClass("active");
+      $("header .navTop").addClass("active");
+      $(".LogoNav2").addClass("active");
+    } else {
+      $("body").css("paddingTop", 0);
+      $("header .nav2").removeClass("active");
+      $("header .navTop").removeClass("active");
+      $(".LogoNav2").removeClass("active");
+    }
+  });
+  $(window).on("load", function () {
+    $("#email").focus();
+  });
+  $(".MobileNav .showLoginRight").on("click", function (e) {
+    e.preventDefault();
+    $(".LoginRight").animate(
+      {
+        right: 0,
+      },
+      500
+    );
+  });
+  $(".fa-xmark").on("click", function (e) {
+    $(".LoginRight").animate(
+      {
+        right: -350,
+      },
+      500
+    );
+  });
+});
